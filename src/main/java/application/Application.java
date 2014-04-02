cd69a6c850286d025d330a9157b3db47bf79e528
Paul Baker <paulbaker@ostk-ubuntu.overstock.com>
2014-04-02 15:24:15 -0600

    Removing cli validation. It's not sexy enough.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -18,65 +18,6 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		// CLI validation
-		String[] files = null;
-		if ((files = validateArguments(args)) != null) {
-			new Application(files[0], files[1]);
-		} else {
-			printUsage();
-		}
-	}
-
-	public static String[] validateArguments(String[] args) {
-		try {
-			String baselineFilePath = null;
-			String testFilePath = null;
-			for (String argument : args) {
-				char argChar = 'h';
-				if (argument.length() > 2) {
-					argChar = argument.charAt(1);
-				}
-				switch (argChar) {
-				case 'b':
-					if (baselineFilePath != null) {
-						throw new YourArgumentIsInvalid();
-					}
-					baselineFilePath = parseArgumentRegex(argument);
-					break;
-				case 't':
-					if (testFilePath != null) {
-						throw new YourArgumentIsInvalid();
-					}
-					testFilePath = parseArgumentRegex(argument);
-					break;
-				case 'h':
-				default:
-					throw new YourArgumentIsInvalid();
-				}
-			}
-			if (baselineFilePath == null || testFilePath == null) {
-				throw new YourArgumentIsInvalid();
-			}
-			return new String[] { baselineFilePath, testFilePath };
-		} catch (YourArgumentIsInvalid e) {
-			return null;
-		}
-	}
-
-	public static String parseArgumentRegex(String argument) throws YourArgumentIsInvalid {
-		String result = "";
-		Pattern pattern = Pattern.compile("^-[a-z]{1}=(.+)$", Pattern.CASE_INSENSITIVE);
-		Matcher matcher = pattern.matcher(argument);
-		if (matcher.find()) {
-			result = matcher.group(1);
-		} else {
-			throw new YourArgumentIsInvalid();
-		}
-		return result;
-	}
-
-	private static void printUsage() {
-		// because we're helpful.
-		System.out.println("You're doing it wrong");
+		
 	}
 }
