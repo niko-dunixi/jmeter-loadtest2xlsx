4f8f2c7451fbfe15df997eb76dc9574f08847fd2
Paul Baker <paulbaker@ostk-ubuntu.overstock.com>
2014-04-03 11:57:24 -0600

    changed parameters

--- lazy-build-run.sh
+++ lazy-build-run.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 #mvn clean install package
 mvn clean package
-#java -jar ./target/jmeter-csv-parser.jar -b=/home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140328-0600_STGREQ.csv -t=/home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140328-1136_SpanishLoadTest-90min.csv
-java -jar ./target/jmeter-csv-parser.jar -b=/home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140401-2100_9-PM-TEST.csv -t=/home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140402-1227_HotRoll-3hr.csv
+java -jar ./target/jmeter-csv-parser.jar -b=/home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140401-2100_9-PM-TEST.csv -t=/home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140402-1730_RetestOfSolr.csv
+#java -jar ./target/jmeter-csv-parser.jar /home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140401-2100_9-PM-TEST.csv /home/paulbaker/LoadTest/Results/JMeter/JMeter/JmeterRawResults_20140402-2100_9-PM-TEST_summary.csv
